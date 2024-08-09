0644d84cf9b1474c993d02b2cb47157ac54c0f8c
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-09 22:21:34 +0100

    chore: The builder can now build main and optional mods.

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,54 @@
-const {readFileSync, unlinkSync, rmSync, cpSync} = require("fs");
-const {join} = require("path");
-const {execSync} = require("child_process");
-require("dotenv").config();
+const { readFileSync, unlinkSync, mkdirSync, cpSync, rmSync } = require("fs");
+const { join } = require("path");
+const { execSync } = require("child_process");
+const { DOMParser } = require('xmldom');
+const xpath = require('xpath');
 
-const packageJsonPath = join(__dirname, "..", "package.json");
-const packageJsonRaw = readFileSync(packageJsonPath, "utf8");
-const packageJson = JSON.parse(packageJsonRaw);
-const version = packageJson.version;
-const buildZip = `${process.env.NO_COMPASS} ${version}.7z`;
-const srcDir = join(__dirname, "..", "src");
-const distDir = join(__dirname, "..", "dist");
-const buildDir = join(distDir, process.env.NO_COMPASS);
+const validArgs = ["main", "steel-ui"];
+const arg = process.argv[2];
 
-try {
-    unlinkSync(buildZip);
-    rmSync(distDir, {recursive: true});
-} catch (e) {
-    // Expected behaviour if it doesn't exist.
+if (!arg) {
+    console.error("Error: Missing required argument. Please provide 'main' or 'steel-ui' as an argument.");
+    process.exit(1);
 }
 
-cpSync(srcDir, buildDir, {recursive: true});
+if (!validArgs.includes(arg)) {
+    console.error(`Error: Invalid argument '${arg}'. Acceptable values are 'main' or 'steel-ui'.`);
+    process.exit(1);
+}
 
-execSync(`.\\node_modules\\7z-bin\\win32\\7z.exe a "${buildZip}" "${buildDir}"`);
+const baseDir = join(__dirname, `../src/${arg}`);
+const xmlFilePath = join(baseDir, "ModInfo.xml");
+
+// Rest of the script
+const xmlRaw = readFileSync(xmlFilePath, "utf8");
+const doc = new DOMParser().parseFromString(xmlRaw, 'text/xml');
+const versionNode = xpath.select1("/xml/Version/@value", doc);
+const modNameNode = xpath.select1("/xml/Name/@value", doc);
+
+const version = versionNode.value;
+const modName = modNameNode.value;
+
+const artifact = `${modName}_${version}.7z`;
+const distDir = join(__dirname, "..", "dist");
+
+try {
+    rmSync(distDir, { recursive: true });
+} catch (e) {
+    // directory does not exist
+}
+
+try {
+    unlinkSync(artifact);
+} catch (e) {
+    // artifact does not exist
+}
+
+const modDistDir = join(distDir, modName);
+const srcDir = baseDir; // Use the argument-based directory here
+
+mkdirSync(modDistDir, { recursive: true });
+
+cpSync(srcDir, modDistDir, { recursive: true });
+
+execSync(`.\\node_modules\\7z-bin\\win32\\7z.exe a "${artifact}" "${modDistDir}"`);
